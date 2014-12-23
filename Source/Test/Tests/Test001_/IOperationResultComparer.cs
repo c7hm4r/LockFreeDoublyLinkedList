01c9cf645706c6656dc92a97a1b233ce079a879b
Christoph Müller <iblzm@hotmail.de>
2014-12-24 00:24:41 +0100

    Added license text to test C# files

--- Source/Test/Tests/Test001_/IOperationResultComparer.cs
+++ Source/Test/Tests/Test001_/IOperationResultComparer.cs
@@ -1,4 +1,18 @@
-﻿namespace Test.Tests.Test001_
+﻿//Copyright 2014 Christoph Müller
+
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+
+//   http://www.apache.org/licenses/LICENSE-2.0
+
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+namespace Test.Tests.Test001_
 {
     abstract class OperationResultComparer<TOperation, TLfdllResult, TLlResult>
         : IOperationResultComparer
